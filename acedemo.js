1c90abf70a5c53c59cbac6e2cd501adc0faaee8e
Madhav <madhav.ksv@gmail.com>
2019-11-22 17:45:03 +0530

    Made some improvements to editor. Added comment, Undo, Redo buttons and functionality.

--- acedemo.js
+++ acedemo.js
@@ -1,71 +1,30 @@
-
-
-
 var editor;
-// Hook up ACE editor to all textareas with data-editor attribute
-function txtareaClick() {
-        // Make Editor-Screen equal to height and width of textarea
-        var textarea = $('#txtarea');
-        var editScr = $('#Editor-Screen');
-        var editDiv = $('#editArea');
-        //editScr.attr({
-        //     width: textarea.width(),
-        //     height: textarea.height(),
-        // });
-        editScr.css("width", textarea.width());
-        editScr.css("height", textarea.height());
-        console.log(editScr);
-        // Bring textarea into the editArea
-        // TBD: Make textarea into 100%
 
-        textarea.css('display', 'none');
-        editScr.css('display', 'block');
-        editor = ace.edit(editDiv[0]);
-        editor.renderer.setShowGutter(true);
-        var textAreaVal =  textarea.val();
-        if (!textAreaVal.endsWith("\n")) {
-            textAreaVal += '\n'
-        }
-        editor.getSession().setValue(textAreaVal);
-        editor.getSession().setMode("ace/mode/javascript");
-        editor.setTheme("ace/theme/tomorrow");
-        editor.setOptions({
-            enableBasicAutocompletion: true,
-            enableSnippets: true,
-            enableLiveAutocompletion: true
-        });
-        console.log(editor);
-        // copy back to textarea on form submit...
-        // textarea.closest('form').submit(function () {
-        //     textarea.val(editor.getSession().getValue());
-        // })
-};
-
-
-
-function appendStringInEditor(appendOp){
-    var curText = editor.getSession().getValue();
+function appendStringInEditor(appendOp) {
+    // var curText = editor.getSession().getValue();
     switch (appendOp) {
         case 'ifCond':
-            textString= "if (i==1) {\n\n}\n"
+            textString = "if (i==1) {\n\n}\n"
             break;
         case 'ifelseCond':
-            textString= "if (i==1) {\n\n} else {\n\n}\n"
+            textString = "if (i==1) {\n\n} else {\n\n}\n"
             break;
         case 'forLoop':
-            textString= "for(i=0;i<10;i++){\n\n}\n"
+            textString = "for(i=0;i<10;i++){\n\n}\n"
             break;
         case 'varAssign':
-            textString= "var i=1;\n"
+            textString = "var i=1;\n"
             break;
         case 'wrongCode':
-            textString= ",,,;%fjdkf,,\n"
+            textString = ",,,;%fjdkf,,\n"
+            break; 
+        case 'comment':
+            textString = "// Some Random Comment\n"
             break;
-        
         default:
-            textString= "";
+            textString = "";
     }
-    editor.getSession().setValue(curText + textString);
+    editor.insert(textString);
 }
 function ifbtnClick() {
     appendStringInEditor('ifCond');
@@ -75,6 +34,9 @@ function ifelsebtnClick() {
 }
 function forbtnClick() {
     appendStringInEditor('forLoop');
+}
+function commentClick() {
+    appendStringInEditor('comment');
 }
 function varbtnClick() {
     appendStringInEditor('varAssign');
@@ -86,17 +48,24 @@ function helpbtnClick() {
     window.alert('Type in Code in the Editor\nYou can use the buttons in the left panel\n\
     Save button saves the code into textarea if no syntax errors are present\n\
     Use Discard button to exit without yout changes\n');
-    
+
 }
 
+function undoClick(){
+    editor.undo();
+}
+
+function redoClick(){
+    editor.redo();
+}
 
 function saveClick() {
     var textarea = $('#txtarea');
     var editScr = $('#Editor-Screen');
-    var annot=editor.getSession().getAnnotations();
+    var annot = editor.getSession().getAnnotations();
     console.log(annot);
-    for (i=0;i<annot.length;i++){
-        if(annot[i].type == 'error'){
+    for (i = 0; i < annot.length; i++) {
+        if (annot[i].type == 'error') {
             window.alert('Syntax errors are present\n Please recheck the code');
             return;
         }
@@ -115,3 +84,45 @@ function discardClick() {
     textarea.css('display', 'block');
 
 }
+
+// Hook up ACE editor to all textareas with data-editor attribute
+function txtareaClick() {
+    // Make Editor-Screen equal to height and width of textarea
+    var textarea = $('#txtarea');
+    var editScr = $('#Editor-Screen');
+    var editDiv = $('#editArea');
+    //editScr.attr({
+    //     width: textarea.width(),
+    //     height: textarea.height(),
+    // });
+    editScr.css("width", textarea.width());
+    editScr.css("height", textarea.height());
+    console.log(editScr);
+    // Bring textarea into the editArea
+    // TBD: Make textarea into 100%
+
+    textarea.css('display', 'none');
+    editScr.css('display', 'block');
+    editor = ace.edit(editDiv[0]);
+    editor.renderer.setShowGutter(true);
+    editor.getSession().setMode("ace/mode/javascript");
+    editor.setTheme("ace/theme/tomorrow");
+    editor.setOptions({
+        enableBasicAutocompletion: true,
+        enableSnippets: true,
+        enableLiveAutocompletion: true
+    });
+    
+    let txtareatxt = textarea.val();
+    if (!txtareatxt.endsWith('\n')){
+        txtareatxt += '\n';
+    }
+    editor.setValue(txtareatxt);
+    editor.clearSelection();
+
+    console.log(editor);
+    // copy back to textarea on form submit...
+    // textarea.closest('form').submit(function () {
+    //     textarea.val(editor.getSession().getValue());
+    // })
+};
